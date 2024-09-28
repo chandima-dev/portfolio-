a1724f35ab6ac5e8c72e696598a0d3ea1d786b5f
IT20230692 <IT20230692@my.sliit.lk>
2024-09-29 00:38:16 +0530

    update team member

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -63,13 +63,21 @@ const AboutUs = () => {
       credentials: 'MIE (SL), C. Eng (ECSL)',
       image: '/images/profile/avator.jpg', // Use different images for different members
     },
+    {
+      name: 'Chandima Sooriyaarachchi',
+      role: 'Managing Director',
+      position: 'Senior Structural Engineer',
+      education: 'BSc Hons Civil Engineering',
+      credentials: 'AMIE (SL), A. Eng (ECSL), AMSSE (SL)',
+      image: '/images/profile/avator.jpg', // Use different images for different members
+    },
   ];
 
   return (
     <div>
       <SubSlider 
         title="Who We Are" 
-        description="We are Structural Engineering Consultants" 
+        description="We are an innovative Architectural and Engineering Design Firm" 
       />
       <div className={styles.aboutUsContainer}>
         <div className={styles.aboutUsContent}>
