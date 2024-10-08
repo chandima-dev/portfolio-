4f7a6ff4e9321bcbf99b301e1262a4b17ef338d4
IT20230692 <IT20230692@my.sliit.lk>
2024-10-09 00:56:04 +0530

    update projects details

--- src/pages/Projects/ProjectData.js
+++ src/pages/Projects/ProjectData.js
@@ -1,62 +1,62 @@
 // Projectdata.js
 const cardData = [
   {
-    image: "/public/images/services/Architectural Design/ad (1).webp",
+    image: "/images/services/Architectural Design/ad (1).webp",
     title: "Architectural Design",
     delay: 100
   },
   {
-    image: "/public/images/services/Architectural Design/ad (2).webp",
+    image: "/images/services/Architectural Design/ad (2).webp",
     title: "Interior Design",
     delay: 300
   },
   {
-    image: "/public/images/services/Architectural Design/ad (3).webp",
+    image: "/images/services/Architectural Design/ad (3).webp",
     title: "Landscape Design",
     delay: 500
   },
   {
-    image: "/public/images/services/Architectural Design/ad (4).webp",
+    image: "/images/services/Architectural Design/ad (4).webp",
     title: "Urban Design",
     delay: 700
   },
   {
-    image: "/public/images/services/Architectural Design/ad (5).webp",
+    image: "/images/services/Architectural Design/ad (5).webp",
     title: "Sustainable Design",
     delay: 900
   },
   {
-    image: "/public/images/services/Architectural Design/ad (6).webp",
+    image: "/images/services/Architectural Design/ad (6).webp",
     title: "Ecological Design",
     delay: 1100
   },
   {
-    image: "/public/images/services/Architectural Design/ad (7).webp",
+    image: "/images/services/Architectural Design/ad (7).webp",
     title: "Commercial Design",
     delay: 1200
   },
   {
-    image: "/public/images/services/Engineering Design/ed (1).webp",
+    image: "/images/services/Engineering Design/ed (1).webp",
     title: "Residential Design",
     delay: 1300
   },
   {
-    image: "/public/images/services/Engineering Design/ed (2).webp",
+    image: "/images/services/Engineering Design/ed (2).webp",
     title: "Healthcare Design",
     delay: 1400
   },
   {
-    image: "/public/images/services/Engineering Design/ed (3).webp",
+    image: "/images/services/Engineering Design/ed (3).webp",
     title: "Educational Design",
     delay: 1500
   },
   {
-    image: "/public/images/services/Engineering Design/ed (4).webp",
+    image: "/images/services/Engineering Design/ed (4).webp",
     title: "Recreational Design",
     delay: 1600
   },
   {
-    image: "/public/images/services/Engineering Design/ed (5).webp",
+    image: "/images/services/Engineering Design/ed (5).webp",
     title: "Cultural Design",
     delay: 1700
   }
