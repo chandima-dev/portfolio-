5041a2671bf68b47b4956fa27c8baca3fc41ff8a
IT20230692 <IT20230692@my.sliit.lk>
2024-10-16 14:17:45 +0530

    update footer details

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,38 +1,47 @@
 import styles from './Footer.module.css';  // Ensure you have the CSS module set up
 import { FaMapMarkerAlt, FaEnvelope, FaPhone, FaFacebook, FaLinkedin, FaRss } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
+  const handleLinkClick = () => {
+    // Custom link click handler logic if needed
+    console.log('Link clicked');
+  };
+
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
         <div className={styles.footerSection}>
           <h4>Contact Info</h4>
-          <br></br>
-          <p><FaMapMarkerAlt /> 1526/6, Crest Field Garden, Malambe Rd, Kottawa</p>
-          <p><FaEnvelope /> <a href="mailto:info@chanlk.com">info@chalk.com</a></p>
-          <p><FaPhone /> <a href="tel:+94705901816">+94 70 590 1816</a></p>
-          <p><FaPhone /> <a href="tel:+94705901816">+94 70 590 1816</a></p>
+          <br />
+          <p><FaMapMarkerAlt /> No. 121/C, Surigama, Kadawatha 11850, Sri Lanka</p>
+          <p><FaEnvelope /> <a href="mailto:falconengnarchitects@gmail.com">falconengnarchitects@gmail.com</a></p>
+          <p><FaPhone /> <a href="tel:+94728130647">+94 72 813 0647</a></p>
+          <p><FaPhone /> <a href="tel:+94770155918">+94 77 015 5918</a></p>
         </div>
+
         <div className={styles.footerSection}>
           <h4>About Company</h4>
           <ul>
-            <li><a href="#about">About Us</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#services">Contact Us</a></li>
+            <li><Link to="/aboutus" onClick={handleLinkClick}>About Us</Link></li>
+            <li><Link to="/projects" onClick={handleLinkClick}>Projects</Link></li>
+            <li><Link to="/services" onClick={handleLinkClick}>Services</Link></li>
           </ul>
         </div>
+
         <div className={styles.footerSection}>
           <h4>Support</h4>
           <ul>
-            <li><a href="#help">Help Center</a></li>
-            <li><a href="#faq">FAQ</a></li>
-            <li><a href="#contact">Contact Us</a></li>
-            <li><a href="#book">Book a Call</a></li>
+            <li><Link to="/contactus" onClick={handleLinkClick}>Help Center</Link></li>
+            <li><Link to="/contactus" onClick={handleLinkClick}>FAQ</Link></li>
+            <li><Link to="/contactus" onClick={handleLinkClick}>Contact Us</Link></li>
+            <li><Link to="/contactus" onClick={handleLinkClick}>Book a Call</Link></li>
           </ul>
         </div>
+
         <div className={styles.footerSection}>
           <h4 className={styles.newsletterTitle}>Newsletter</h4>
-          <br></br>
+          <br />
           <p>Sign up for our newsletter to receive updates, news, and knowledge base.</p>
           <form className={styles.newsletterForm}>
             <input type="email" placeholder="Enter your email" required />
@@ -45,6 +54,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
+
       <div className={styles.copyright}>
         <p>&copy; {new Date().getFullYear()} Falcon Engineering. All rights reserved.</p>
       </div>
